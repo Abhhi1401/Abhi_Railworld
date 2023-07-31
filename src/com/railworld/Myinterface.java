b505a5a45367151de962829727ed67aa29f816cb
Abhhi1401 <bhoiabhishek1401@gmail.com>
2023-07-31 14:57:24 +0530

    day 16 assingment

---
+++ src/com/railworld/Myinterface.java
@@ -0,0 +1,14 @@
+package com.railworld;
+
+public interface Myinterface {
+	
+	public void fun();
+	
+	public static void info() {
+		
+	}
+	default String funA() {
+		return "this is the default method of Myinterface";
+	}
+
+}
